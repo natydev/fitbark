4dfa53ea1fcf4dc5fdae44b03147bc1022c7ac68
NatyDev <natydev@localhost>
2019-01-29 18:20:00 +0100

    Add initial gem base files

---
+++ lib/fitbark.rb
@@ -0,0 +1,6 @@
+require "fitbark/version"
+
+module Fitbark
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fitbark_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Fitbark do
+  it "has a version number" do
+    expect(Fitbark::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
