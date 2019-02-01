f7c71a311125e29cfe05eb94a7ef92464d10db24
NatyDev <natydev@localhost>
2019-02-01 22:56:34 +0100

    Add Fitbark::Errors module
    define a BaseError parent class and add first FetchTokenError

---
+++ lib/fitbark/errors.rb
@@ -0,0 +1,27 @@
+module Fitbark
+  # this module provide error classes
+  module Errors
+    # common base error class
+    class BaseError < StandardError
+      def initialize(args = {})
+        @message = args.fetch(:message, nil)
+        @code = args.fetch(:code, 400)
+      end
+
+      attr_accessor :message, :code
+
+      def to_s
+        "#{code} - #{message}"
+      end
+    end
+
+    # FetchTokenError
+    class FetchTokenError < BaseError; end
+    # TokenNotProvidedError
+    class TokenNotProvidedError < BaseError
+      def initialize
+        @message = 'Token is required for this operation'
+      end
+    end
+  end
+end
