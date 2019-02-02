5b86db7daaf7ef806d85329b7dca92a51347ec0f
NatyDev <natydev@localhost>
2019-02-02 18:54:09 +0100

    Add Fitbark::Constants module

---
+++ lib/fitbark/constants.rb
@@ -0,0 +1,11 @@
+module Fitbark
+  # this module provide constants support
+  module Constants
+    API_SCHEME = 'https'.freeze
+    API_HOST = 'app.fitbark.com'.freeze
+    API_SUBHOST = 'api/v2'.freeze
+    AUTHORIZE_PATH = '/oauth/authorize'.freeze
+    TOKEN_PATH = '/oauth/token'.freeze
+    TOKEN_INFO_PATH = TOKEN_PATH + '/info'.freeze
+  end
+end
